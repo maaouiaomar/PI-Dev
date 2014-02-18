080854ceb323b57b74b6372f561e4cd70e94f183
montassar40 <mm@mm-PC>
2014-02-18 15:19:07 +0100

    Montassar

--- src/covoiturage/Covoiturage.java
+++ src/covoiturage/Covoiturage.java
@@ -14,7 +14,7 @@ public class Covoiturage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("EST ya dawla");
+        System.out.println("Esprit");
         
     }
 }
