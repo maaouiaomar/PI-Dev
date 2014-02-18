a7fa3cadde4293a3670de72da9384918d6dd8e35
montassar40 <mm@mm-PC>
2014-02-18 15:27:49 +0100

    Montassar

--- src/covoiturage/Covoiturage.java
+++ src/covoiturage/Covoiturage.java
@@ -14,7 +14,7 @@ public class Covoiturage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Esprit");
+        System.out.println("E");
         
     }
 }
